11cc0dbbe8adb6b7c7844c7feaa15ad9f96890c8
unknown <admin@Csnode-09.psrec.ac.in>
2023-07-21 14:12:39 +0530

    king

---
+++ src/world/hell.java
@@ -0,0 +1,16 @@
+package world;
+
+public class hell {
+    String movie;
+    String name;
+    String hero;
+    int budget;
+
+    public void printer(){
+        System.out.println("TheMovieIS:"+ movie);
+        System.out.println("TheHeroIS:"+ hero);
+        System.out.println("TheBudgetIs:"+budget);
+    }
+
+}
+
---
+++ src/world/king.java
@@ -0,0 +1,28 @@
+package world;
+
+public class king {
+    public static void main(String[] args) {
+        hell s1 = new hell();
+        hell s2 = new hell();
+        hell s3 = new hell();
+
+        s1.movie="one piece";
+        s1.hero="luff";
+        s1.budget=5000;
+
+        s2.movie="naruto";
+        s2.budget=6000;
+        s2.hero="naruto";
+
+        s3.movie="jutsutu kaisen";
+        s3.budget=6000;
+        s3.hero="naruto";
+
+        s1.printer();
+        s2.printer();
+        s3.printer();
+
+
+    }
+}
+
